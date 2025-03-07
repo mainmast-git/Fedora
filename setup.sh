fb2ff456be6bb594a69aec3f1ed0c0ef22933bf0
Ramin Samadi <ramin-samadi@outlook.com>
2025-03-07 20:36:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,11 +16,10 @@ sudo dnf update -y
 echo "Installing required packages..."
 sudo dnf install -y \
     vim @virtualization bridge-utils timeshift neovim \
-    qdirstat qt5ct gns3-gui gns3-server libminizip1 \
-    libxcb-xinerama0 tldr fastfetch lsd make gawk \
-    trash-cli fzf bash-completion whois bat tree \
-    ripgrep gnome-tweaks plocate fail2ban fastfetch \
-    gns3-iou papirus-icon-theme epapirus-icon-theme \
+    qdirstat qt5ct gns3-gui gns3-server libxcb-xinerama0 \
+    tldr fastfetch lsd make gawk trash-cli fzf bash-completion \
+    whois bat tree ripgrep gnome-tweaks plocate fail2ban \
+    fastfetch gns3-iou papirus-icon-theme epapirus-icon-theme \
     google-chrome-stable
  
 
